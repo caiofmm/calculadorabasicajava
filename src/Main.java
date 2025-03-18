2a87fdecadcbcf5304c83232de90d4e55ec4e2c1
caiofmm <caiofilipemm@gmail.com>
2025-03-17 23:44:41 -0300

    Adicionando funcoes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Exercicios calculadora");
+        calculadora.soma(3, 7);
+        calculadora.subtracao(10, 7);
+        calculadora.multiplicacao(10, 7);
+        calculadora.divisao(10, 5);
     }
 }
--- src/calculadora.java
+++ src/calculadora.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class calculadora {
+
+    public static void soma(double a, double b) {
+        double resultado = a + b;
+        System.out.println("A soma de " + a + " mais " + b + " é " + resultado);
+    }
+    public static void subtracao(double a, double b) {
+        double resultado = a - b;
+        System.out.println("A subtracao de " + a + " mais " + b + " é " + resultado);
+    }
+    public static void multiplicacao(double a, double b) {
+        double resultado = a * b;
+        System.out.println("A multiplicacao de " + a + " mais " + b + " é " + resultado);
+    }
+    public static void divisao(double a, double b) {
+        double resultado = a / b;
+        System.out.println("A divisao de " + a + " mais " + b + " é " + resultado);
+    }
 }
